1b6f9b8559647426e69f278e186dacee0613304c
Njabulo Majozi <mr.enmajozi@gmail.com>
2019-03-27 10:36:23 +0200

    Add typing function

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func checkWebsiteStatus(website string) string {
-	timeout := time.Duration(1 * time.Second)
+	timeout := time.Duration(10 * time.Second)
 	client := http.Client{
 		Timeout: timeout,
 	}
@@ -22,17 +22,24 @@ func checkWebsiteStatus(website string) string {
 	return (string(resp.StatusCode) + resp.Status)
 }
 
+func typing(responseWriter slacker.ResponseWriter) {
+	responseWriter.Typing()
+	time.Sleep(time.Second)
+}
+
 func main() {
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"))
 
 	bot.Command("Hello", &slacker.CommandDefinition{
 		Handler: func(request slacker.Request, response slacker.ResponseWriter) {
+			typing(response)
 			response.Reply("Hello, I am go-bot\n\nI can do the following:\n- [wstatus] check https://www.mrenmajozi.com status")
 		},
 	})
 
 	bot.Command("wstatus", &slacker.CommandDefinition{
 		Handler: func(request slacker.Request, response slacker.ResponseWriter) {
+			typing(response)
 			response.Reply(checkWebsiteStatus(os.Getenv("MY_WEBSITE")))
 		},
 	})
